e29c0aa55878756c748c4f034edd894a36678451
mephistofel <mephistofel66613@gmail.com>
2019-09-11 14:15:12 +0300

    Routing

--- angular-basics/src/app/app-routing.module.ts
+++ angular-basics/src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BasicsComponent} from './basics/basics.component';
+import {AddFeaturesComponent} from './add-features/add-features.component';
+import {BasicRxjsComponent} from './basic-rxjs/basic-rxjs.component';
+import {ComponentInDetailsComponent} from './component-in-details/component-in-details.component';
+import {DirectivesComponent} from './directives/directives.component';
+import {FormsValidationsComponent} from './forms-validations/forms-validations.component';
+import {HttpClientComponent} from './http-client/http-client.component';
+import {ModulesComponent} from './modules/modules.component';
+import {PipesComponent} from './pipes/pipes.component';
+import {RoutingComponent} from './routing/routing.component';
+import {ServicesComponent} from './services/services.component';
+import {TsForAngularComponent} from './ts-for-angular/ts-for-angular.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BasicsComponent},
+  {path: 'add-features', component: AddFeaturesComponent },
+  {path: 'basic-rxjs', component: BasicRxjsComponent},
+  {path: 'component-in-details', component: ComponentInDetailsComponent},
+  {path: 'directives', component: DirectivesComponent},
+  {path: 'forms-validations', component: FormsValidationsComponent},
+  {path: 'http-client', component: HttpClientComponent},
+  {path: 'modules', component: ModulesComponent},
+  {path: 'pipes', component: PipesComponent},
+  {path: 'routing', component: RoutingComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'ts-for-angular', component: TsForAngularComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
